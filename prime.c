7c3fb9745395d3a7891e6676fe4696f953cafbcb
Gokberk Karaca <gskaraca1905@gmail.com>
2018-02-25 13:43:09 +0300

    Add additional checks for arguments

--- prime.c
+++ prime.c
@@ -1,27 +1,38 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "unistd.h"
+#include "string.h"
 #include "sys/types.h"
 #include "sys/wait.h"
+
+#define END_OF_DATA -1
+#define MAX_INTEGERS 1000000
+#define MAX_CHILDREN 50
 
 int main(int argc, char **argv) {
 
   int numOfIntegers = atoi(argv[1]);
   int numOfChildren = atoi(argv[2]);
-  int integersArray[numOfIntegers - 1];
-  pid_t childProcesses[numOfChildren];
 
-  // Check if the number of arguments are correct
+  // Check if the arguments are correct
   if (argc != 3) {
     printf("Wrong number of arguments\n");
     printf("Usage: prime <numberofintegers> <numberofchildren>");
     exit(1);
   }
-
-  // Create an array that includes number from 2 to N
-  for (int i = 0; i < numOfIntegers - 1; i++) {
-    integersArray[i] = i + 2;
+  else if (numOfIntegers > MAX_INTEGERS) {
+    printf("Invalid number of integers\n");
+    printf("Maximum 1000000 integers are allowed");
+    exit(1);
   }
+  else if(numOfChildren > MAX_CHILDREN) {
+    printf("Invalid number of children\n");
+    printf("Maximum 50 children are allowed");
+    exit(1);
+  }
+
+  pid_t childProcesses[numOfChildren];
+  //pid_t printerProcess;
 
   for (int i = 0; i < numOfChildren; i++) {
     childProcesses[i] = fork();
@@ -29,10 +40,10 @@ int main(int argc, char **argv) {
       fprintf(stderr, "Fork failed");
       exit(-1);
     }
-    else if (childProcesses[i] == 0) {
+    else if (childProcesses[i] == 0) { // Children runs here
       printf("I am a child process %d\n", childProcesses[i]);
     }
-    else {
+    else { // Parent runs here
       wait(NULL);
       printf("Children complete, pid: %d\n", childProcesses[i]);
       exit(0);
